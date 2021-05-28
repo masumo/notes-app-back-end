80ebd31e78e53150641b772521cd96b2070ac49a
masumo <mas.kosumo@gmail.com>
2021-05-28 16:14:37 +0700

    ubah port server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 8080,
+    port: 5000,
     host: process.env.NODE_ENV !== "production" ? "localhost" : "172.31.45.170",
     routes: {
       cors: {
